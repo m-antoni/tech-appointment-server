3c4a2d6d188325dfc2f20ceff5ee964825df5c89
Michael Antoni <michaelantoni.cs@gmail.com>
2022-10-04 03:34:09 +0800

    add queries and mutations in graphql

--- graphql/schema.js
+++ graphql/schema.js
@@ -4,9 +4,12 @@ const {
   GraphQLNonNull,
   GraphQLString,
   GraphQLSchema,
+  GraphQLID,
 } = require('graphql');
+const Appointment = require('../models/Appointment');
+const Client = require('../models/Client');
 const Technician = require('../models/Technician');
-const { TechnicianType } = require('./type-defs');
+const { TechnicianType, ClientType, AppointmentType } = require('./type-defs');
 
 // Root Query
 const RootQuery = new GraphQLObjectType({
@@ -16,6 +19,39 @@ const RootQuery = new GraphQLObjectType({
       type: new GraphQLList(TechnicianType),
       resolve(parent, args) {
         return Technician.find();
+      },
+    },
+    technician: {
+      type: TechnicianType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Technician.findById(args.id);
+      },
+    },
+    clients: {
+      type: new GraphQLList(ClientType),
+      resolve(parent, args) {
+        return Client.find();
+      },
+    },
+    client: {
+      type: ClientType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Client.findById(args.id);
+      },
+    },
+    appointments: {
+      type: new GraphQLList(AppointmentType),
+      resolve(parent, args) {
+        return Appointment.find();
+      },
+    },
+    appointment: {
+      type: AppointmentType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Appointment.findById(args.id);
       },
     },
   },
@@ -40,6 +76,46 @@ const mutation = new GraphQLObjectType({
         });
 
         return technician.save();
+      },
+    },
+    deleteTechnician: {
+      type: TechnicianType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Technician.findByIdAndRemove(args.id);
+      },
+    },
+    addClient: {
+      type: ClientType,
+      args: {
+        name: { type: GraphQLNonNull(GraphQLString) },
+        email: { type: GraphQLNonNull(GraphQLString) },
+        phone: { type: GraphQLNonNull(GraphQLString) },
+        address: { type: GraphQLNonNull(GraphQLString) },
+      },
+      resolve(parent, args) {
+        const client = new Client({
+          name: args.name,
+          email: args.email,
+          phone: args.phone,
+          address: args.address,
+        });
+
+        return client.save();
+      },
+    },
+    deleteClient: {
+      type: ClientType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Client.findByIdAndRemove(args.id);
+      },
+    },
+    deleleAppointment: {
+      type: AppointmentType,
+      args: { id: { type: GraphQLNonNull(GraphQLID) } },
+      resolve(parent, args) {
+        return Appointment.findByIdAndRemove(args.id);
       },
     },
   },
